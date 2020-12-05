cd78ffe79588952f14e48144318a6eeb90f17add
jerrr36 <58893325+jerrr36@users.noreply.github.com>
2020-12-05 10:29:41 -0500

    Working on UI

--- arduino-ui/src/Components/OvenContainer.js
+++ arduino-ui/src/Components/OvenContainer.js
@@ -2,6 +2,7 @@ import React from "react"
 import MyHeader from "./Header"
 import MyThermometer from "./Therm"
 import Timer from "./Timer"
+
 //import ThemingLayout from "./c"
 //import { Button, Container, Divider, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react';
 
@@ -14,22 +15,30 @@ class OvenContainer extends React.Component{
     render(){
         return(
             
-            <div>
+    
                 
-                <div className="body">
+                <body className="bg">
                     <h1 className="header"> Pizza Oven UI </h1>
                         <div className="article-container">
                             <div className="article1">
-                                <MyThermometer />
+                               <div className="datadisp">Current Temp:</div>
                             </div>
                             <div className="article1">
-                                <h3>Timers</h3>
-                                <Timer />
-                                <Timer />
+                                <div className="timer-buttons">
+                                
+                                        <button className="b">Start Timer</button>
+                                    
+                                
+                                        <button className="b">Reset Timer</button>
+                                
+                                </div>
+                                
+                                <div className="timer"> Time Remaining:</div>
+                                
                             </div>
                         </div>
-                </div>
-            </div>
+                </body>
+    
             
                 );
     }
